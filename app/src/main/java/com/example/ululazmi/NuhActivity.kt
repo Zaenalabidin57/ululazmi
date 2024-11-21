ef0fc989b77fcfd32dc7b18d5a8a3eba2e8646f1
ZaenalAbidin57 <rifqifadil57@gmail.com>
2024-11-21 14:48:20 +0700

    inisialisasi desuwa

---
+++ app/src/main/java/com/example/ululazmi/NuhActivity.kt
@@ -0,0 +1,8 @@
+package com.example.ululazmi
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NuhActivity : AppCompatActivity(){
+}
---
+++ app/src/main/java/com/example/ululazmi/Question.kt
@@ -0,0 +1,7 @@
+package com.example.ululazmi
+
+data class Question (
+    val text: String,
+    val answers: List<String>,
+    val correctAnswer: Int
+)
